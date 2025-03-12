03d52d920846379842b7c022d310acdf5ea2bc60
New <ivanenduskin@gmail.com>
2025-03-12 17:28:43 +0400

    File

--- Lab №2.py
+++ Lab №2.py
@@ -4,25 +4,30 @@
 # 1.Входной файл является обыкновенным (т.е. нет требования на «бесконечность» файла);
 # 2.Распознавание и обработку делать  через регулярные выражения;
 # 3.В вариантах, где есть параметр (например К), допускается его заменить на любое число(константу);
+
 import re
 
-def check_number(number):
-    if number % 2 != 0 and number <= 4095:
-        binary_number = format(number, '012b')
-        if re.search(r'000', binary_number):
-            start_position = binary_number.index('000')
-            return True, start_position
+index_dict = {
+    0: "ноль", 1: "один", 2: "два", 3: "три", 4: "четыре",
+    5: "пять", 6: "шесть", 7: "семь", 8: "восемь", 9: "девять",
+}
+
+def check_number(binary_number):
+    if int(binary_number) % 2 != 0 and int(binary_number, 2) <= 4095:
+        count = len(re.findall('000', binary_number))
+        if count == 1:
+            position = binary_number.find('000')
+            return True, position
     return False, 0
 
 def process_file(file_path):
     with open(file_path, 'r') as file:
         for line in file:
-            number = int(line.strip())
-            result, start_position = check_number(number)
+            binary_number = line.strip()
+            result, start_position = check_number(binary_number)
             if result:
-                binary_number = format(number, '012b')
-                transformed_number = ''.join(digit for digit in binary_number if digit != '0')
-                print(f'{transformed_number} {start_position}')
+                transformed_number = re.sub('0', '', binary_number)
+                index_word = index_dict.get(start_position, str(start_position))
+                print(f'{transformed_number} {index_word}')
 
-process_file('input.txt')
-
+process_file('numbers.txt')

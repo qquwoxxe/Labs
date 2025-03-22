3dc82498b6661f1fbb3c168249d9b8a302966026
New <ivanenduskin@gmail.com>
2025-03-22 13:50:17 +0400

    File

--- Lab №2.py
+++ Lab №2.py
@@ -17,21 +17,17 @@ def extract_binary(part):
     return matched.group(0) if matched else None
 
 def check_number(binary_number):
-    try:
-        decimal = int(binary_number, 2)
-        if decimal % 2 != 0 and decimal <= 4095:
-            position = binary_number.find('000')
-            if position != -1 and binary_number.count('000') == 1:
-                return True, position
-        return False, 0
-    except ValueError:
-        return False, 0
+    if int(binary_number, 2) % 2 != 0 and int(binary_number, 2) <= 4095:
+        position = binary_number.find('000')
+        if position != -1 and binary_number.count('000') == 1:
+            return True, position
+    return False, 0
+
 
 def process_file(file_path):
     with open(file_path, 'r') as file:
         for line in file:
-            parts = line.strip().split()
-            for part in parts:
+            for part in line.strip().split():
                 binary_number = extract_binary(part)
                 if binary_number:
                     result, start_position = check_number(binary_number)
@@ -39,5 +35,6 @@ def process_file(file_path):
                         transformed_number = binary_number.replace('0', '')
                         index_word = index_dict.get(start_position, str(start_position))
                         print(f'{transformed_number} {index_word}')
+
 
 process_file('numbers.txt')
